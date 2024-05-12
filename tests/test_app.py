8a4d179534bbdc6cfdd289bc313ecd200544d1a7
K-dash <kdash11011227@gmail.com>
2024-05-12 14:35:15 +0900

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root():
+    # AAA parttern
+    client = TestClient(app)    # Arrange
+    response = client.get("/")  # Act
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {"message": "Hello World!"}    # Assert
