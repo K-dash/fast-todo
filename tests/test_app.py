b38a00b46bf1d51887d808f6532eec2161d5cd38
K-dash <kdash11011227@gmail.com>
2024-05-12 14:43:02 +0900

    Change: Rename repository name.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,12 +2,12 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+from fast_todo.app import app
 
 
 def test_read_root():
     # AAA parttern
-    client = TestClient(app)    # Arrange
+    client = TestClient(app)  # Arrange
     response = client.get("/")  # Act
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {"message": "Hello World!"}    # Assert
+    assert response.json() == {"message": "Hello World!"}  # Assert
