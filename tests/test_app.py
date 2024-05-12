a9d48a399990955ec26f3bda65763e94dd96b4bd
K-dash <kdash11011227@gmail.com>
2024-05-12 16:31:28 +0900

    Add endpoint to retrieve a single user and corresponding test code

--- tests/test_app.py
+++ tests/test_app.py
@@ -35,6 +35,22 @@ def test_read_users(client):
     }
 
 
+def test_read_user(client):
+    response = client.get("/users/1")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "johndoe",
+        "email": "QpC7U@example.com",
+    }
+
+
+def test_read_user_not_found(client):
+    response = client.get("/users/5")
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
 def test_update_user(client):
     response = client.put(
         "/users/1",
